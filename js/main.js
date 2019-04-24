4a66ff3c3281357063cc741a503b065e5f87d8d7
vladislav4eg1995 <vladyslav.hubenia@oa.edu.ua>
2019-04-24 16:14:44 +0300

    init

---
+++ js/main.js
@@ -0,0 +1,179 @@
+function tranformToX(e){return e.preventDefault(),openOverlay(),$(this).addClass("close"),$(this).addClass("x"),$(this).unbind("click",tranformToX),$(this).bind("click",tranformToBurger),!1}function tranformToBurger(e){return e.preventDefault(),closeOverlay(),$(this).removeClass("close"),$(this).removeClass("x"),$(this).unbind("click",tranformToBurger),$(this).bind("click",tranformToX),!1}function openOverlay(){$("#overlay").css({visibility:"visible"})}function closeOverlay(){$("#overlay").css({visibility:"hidden"})}$(".lines-button.arrow.arrow-left").bind("click",tranformToX),
+new Swiper(".swiper-showerdoor",{
+    spaceBetween:10,
+    slidesPerView:1,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    breakpoints:{
+        320:{
+            slidesPerView:1,
+            spaceBetween:0
+        },
+        480:{
+            slidesPerView:1,
+            spaceBetween:0
+        },
+        576:{slidesPerView:1,
+            spaceBetween:0
+        },
+        768:{slidesPerView:1,
+            spaceBetween:0
+        },
+        992:{slidesPerView:1,
+            spaceBetween:10
+        },
+        1200:{slidesPerView:1,
+            spaceBetween:10
+        }
+        }
+    }),
+    
+new Swiper(".swiper-showerscreen",{
+    spaceBetween:10,
+    slidesPerView:1,
+    navigation:{
+    nextEl:".swiper-button-next",
+    prevEl:".swiper-button-prev"
+},
+breakpoints:{
+    320:{
+        slidesPerView:1,
+        spaceBetween:0
+    },
+    480:{
+        slidesPerView:1,
+        spaceBetween:0
+    },
+    576:{slidesPerView:1,
+        spaceBetween:0
+    },
+    768:{slidesPerView:1,
+        spaceBetween:0
+    },
+    992:{slidesPerView:1,
+        spaceBetween:10
+    },
+    1200:{slidesPerView:1,
+        spaceBetween:10
+    }
+    }
+}),
+new Swiper(".swiper-showercurtain",{
+    spaceBetween:10,
+    slidesPerView:1,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"
+    },
+    breakpoints:{
+        320:{
+            slidesPerView:1,
+            spaceBetween:0
+        },
+        480:{
+            slidesPerView:1,
+            spaceBetween:0
+        },
+        576:{slidesPerView:1,
+            spaceBetween:0
+        },
+        768:{slidesPerView:1,
+            spaceBetween:0
+        },
+        992:{slidesPerView:1,
+            spaceBetween:10
+        },
+        1200:{slidesPerView:1,
+            spaceBetween:10
+        }
+        }
+    }),
+new Swiper(".swiper-ourwork",{
+    slidesPerView:3.5,
+    spaceBetween:30,
+    loop:!0,
+    navigation:{
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev"},
+    breakpoints:{
+        320:{slidesPerView:1,spaceBetween:0},
+        540:{slidesPerView:1,spaceBetween:10},
+        720:{slidesPerView:2,spaceBetween:10},
+        960:{slidesPerView:2,spaceBetween:30},
+        1200:{slidesPerView:3,spaceBetween:30}
+    }}),
+new Swiper(".swiper-reviews",{
+    slidesPerView:3.5,
+    spaceBetween:30,
+    navigation:{
+        nextEl:".swiper-button-next.swiper-reviews-next",
+        prevEl:".swiper-button-prev.swiper-reviews-prev"},
+    centeredSlides:!0,
+    loop:!0,
+    breakpoints:{
+        320:{
+            slidesPerView:1,
+            spaceBetween:0},
+        480:{
+            slidesPerView:1,
+            spaceBetween:10},
+        640:{slidesPerView:1.5,
+            spaceBetween:20},
+        770:{slidesPerView:1.5,
+            spaceBetween:10},
+        930:{slidesPerView:2.5,
+            spaceBetween:30},
+        1200:{slidesPerView:3.5,
+            spaceBetween:30}
+        }
+    });
+    var inputs = document.querySelectorAll('.inputfile');
+    Array.prototype.forEach.call(inputs, function (input) {
+        var label = input.nextElementSibling,
+            labelVal = label.innerHTML;
+        input.addEventListener('change', function (e) {
+            var fileName = '';
+            if (this.files && this.files.length > 1)
+                fileName = (this.getAttribute('data-multiple-caption') || '').replace('{count}', this.files.length);
+            else
+                fileName = e.target.value.split('\\').pop();
+            if (fileName)
+                label.querySelector('span').innerHTML = fileName;
+            else
+                label.innerHTML = labelVal;
+        });
+    });
+
+
+    // send form
+// Как только страничка загрузилась 
+window.onload = function () {
+    // проверяем поддерживает ли браузер FormData 
+    if (!window.FormData) {
+        alert("Браузер не поддерживает загрузку файлов на этом сайте");
+    }
+}
+$(document).ready(function () {
+    // =validation
+    var errorTxt = 'Ошибка отправки';
+    $("#sendform").validate({
+        submitHandler: function (form) {
+            var form = document.forms.sendform,
+                formData = new FormData(form),
+                xhr = new XMLHttpRequest();
+
+            xhr.open("POST", "http://dev1.silk-its.com/tvist/");
+
+            xhr.onreadystatechange = function () {
+                if (xhr.readyState == 4) {
+                    if (xhr.status == 200) {
+                        $("#sendform").html('<p class="thank">Данные отправлены!<p>');
+                    }
+                }
+            };
+            xhr.send(formData);
+        }
+    });
+})
---
+++ send.php
@@ -0,0 +1,66 @@
+<?php
+//recipient
+
+$to = "Elena357910@yandex.com";  
+
+
+$subject = "Форма отправки сообщений с сайта Твист";
+    
+//sender
+$from = "Elena357910@yandex.com"; 
+$fromName = 'Tvist';
+
+//email subject
+$subject = 'PHP Email with Attachment by CodexWorld'; 
+
+//attachment file path
+$email = $_POST['email'];
+$phone = $_POST['phone'];
+$descr = $_POST['descr'];
+$file = $_FILES['file'];
+
+//email body content
+$htmlContent = '<h1>PHP Email with Attachment by CodexWorld</h1>
+    <p>This email has sent from PHP script with attachment.</p>';
+
+//header for sender info
+$headers = "From: $fromName"." <".$from.">";
+
+//boundary 
+$semi_rand = md5(time()); 
+$mime_boundary = "==Multipart_Boundary_x{$semi_rand}x"; 
+
+//headers for attachment 
+$headers .= "\nMIME-Version: 1.0\n" . "Content-Type: multipart/mixed;\n" . " boundary=\"{$mime_boundary}\""; 
+
+//multipart boundary 
+$message = "--{$mime_boundary}\n" . "Content-Type: text/html; charset=\"UTF-8\"\n" .
+"Content-Transfer-Encoding: 7bit\n\n" . $htmlContent . "\n\n"; 
+
+//preparing attachment
+if(!empty($file) > 0){
+    if(is_file($file)){
+        $message .= "--{$mime_boundary}\n";
+        $fp =    @fopen($file,"rb");
+        $data =  @fread($fp,filesize($file));
+
+        @fclose($fp);
+        $data = chunk_split(base64_encode($data));
+        $message .= "Content-Type: application/octet-stream; name=\"".basename($file)."\"\n" . 
+        "Content-Description: ".basename($file)."\n" .
+        "Content-Disposition: attachment;\n" . " filename=\"".basename($file)."\"; size=".filesize($file).";\n" . 
+        "Content-Transfer-Encoding: base64\n\n" . $data . "\n\n";
+    }
+}
+$message .= "Email: ". $email; 
+$message .= "Телефон: ". $phone; 
+$message .= "Сообщение: ". $descr; 
+$message .= "--{$mime_boundary}--";
+$returnpath = "-f" . $from;
+
+//send email
+$mail = @mail($to, $subject, $message, $headers, $returnpath); 
+
+//email sending status
+echo $mail?"<h1>Mail sent.</h1>":"<h1>Mail sending failed.</h1>";
+?>
