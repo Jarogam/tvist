528c54465c8f5245c4dde3c11f476ce0c4f83d7a
transoman <vasiag10@gmail.com>
2019-04-24 17:15:49 +0300

    Change swiper navigation styles.

--- js/main.js
+++ js/main.js
@@ -98,9 +98,9 @@ new Swiper(".swiper-ourwork",{
         nextEl:".swiper-button-next",
         prevEl:".swiper-button-prev"},
     breakpoints:{
-        320:{slidesPerView:1,spaceBetween:0},
-        540:{slidesPerView:1,spaceBetween:10},
-        720:{slidesPerView:2,spaceBetween:10},
+        // 320:{slidesPerView:1,spaceBetween:0},
+        // 540:{slidesPerView:1,spaceBetween:10},
+        767:{slidesPerView:1,spaceBetween:10},
         960:{slidesPerView:2,spaceBetween:30},
         1200:{slidesPerView:3,spaceBetween:30}
     }}),
